24291816dfa9f2a5e04ca64b299c7b6f87a89dee
Kulanos <axcalibur@mail.ru>
2024-06-18 18:14:04 +0300

    reliz_1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean1 == bean2);
+
+        ApplicationContext applicationContext2 = new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat bean3 = (Cat) applicationContext2.getBean("cat");
+        Cat bean4 = (Cat) applicationContext2.getBean("cat");
+        System.out.println(bean3.getName());
+        System.out.println(bean3 == bean4);
     }
 }
